d44a8b23821f5c6a115289132d513f28604f8dfe
Madelene Campos <madelenecampos@gmail.com>
2016-08-26 11:49:13 -0400

    Adds button and ability to click button and add new post

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ angular.module('shlapperNews', [])
 .controller('MainCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!'; // declares a variable called "test"
+  $scope.test = "Maddie's first Angular app!"; // declares a variable called "test"
   $scope.posts = [  // new scope variable called "posts"
   {title: 'post 1', upvotes: 5},
   {title: 'post 2', upvotes: 2},
@@ -10,4 +10,10 @@ function($scope){
   {title: 'post 4', upvotes: 9},
   {title: 'post 5', upvotes: 4}
  ];
+
+ $scope.addPost = function(){
+  $scope.posts.push({title: 'This is my new post', upvotes: 0});
+ };
 }]);
+
+
