073d3a84f38cfbbb7a49f324ecdb35de1699a604
Madelene Campos <madelenecampos@gmail.com>
2016-09-06 10:43:37 -0400

    Binds .posts variable in controller to posts array in service

--- app.js
+++ app.js
@@ -12,14 +12,8 @@ angular.module('schlapperNews', [])
 'posts',
 function($scope, posts){
   $scope.test = "Maddie's first Angular app!"; // declares a variable called "test"
-  $scope.posts = [  // new scope variable called "posts"
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4}
- ];
-
+  $scope.posts = posts.posts;
+ 
  $scope.addPost = function(){
   if(!$scope.title || $scope.title === '') { return; }
   $scope.posts.push({
